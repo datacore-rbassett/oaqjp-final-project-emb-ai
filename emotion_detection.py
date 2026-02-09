c5e01222d9ac6ee3cf808fe9cd49235755042bff
datacore-rbassett <rbassett314159@gmail.com>
2026-02-09 16:14:37 -0500

    Return a focused dict with dominant_emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     ''' analyze emotion/sentiment of given text, 
@@ -8,4 +9,14 @@ def emotion_detector(text_to_analyze):
     headers = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=headers, json=obj)
-    return response.text
+    formatted_text = json.loads(response.text)
+    emotions = formatted_text['emotionPredictions'][0]['emotion']
+    max_score = 0
+    dominant_emotion = None
+    for k, v in emotions.items():
+        if max_score < v:
+            max_score = v
+            dominant_emotion = k
+    r = emotions.copy()
+    r['dominant_emotion'] = dominant_emotion
+    return r
