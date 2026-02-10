6d4a8013982004f6d5055b28c22539db96fd4b90
datacore-rbassett <rbassett314159@gmail.com>
2026-02-09 21:04:24 -0500

    Add web app server

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, request, url_for
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+def print_comma_list(x):
+    """ convert a list into a comma-separated string, with the last 
+        two items separated with 'and'.
+    """
+    if not x:
+        s = ""
+    elif len(x) == 1:
+        s = str(x[0])
+    else:
+        s = ", ".join(x[:-1])
+        s = f"{s} and {x[-1]}"
+    return s
+
+@app.route('/emotionDetector')
+def emotion_analyzer():
+    result = emotion_detector(request.args.get("textToAnalyze"))
+    list = [f"{k}: {v}" for k, v in result.items() if k != 'dominant_emotion']
+    output = print_comma_list(list)
+    text = f'For the given statement, the system response is {output}.'
+    text = text + f'The dominant emtion is <strong>{result["dominant_emotion"]}</strong>.'
+    return text
